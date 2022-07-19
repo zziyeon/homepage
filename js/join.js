596e1f67eb149a268fbd1d4d41a06a19d4877b32
zziyeon <jiyunsara99@naver.com>
2022-07-19 23:41:31 +0900

    추가사항

---
+++ js/join.js
@@ -0,0 +1,25 @@
+// var user_state = document.getElementsByClassName("user_state");
+
+// function handleClick(event) {
+//   console.log(event.target);
+
+//   console.log(event.target.classList);
+
+//   if (event.target.classList[1] === "clicked") {
+//     event.target.classList.remove("clicked");
+//   } else {
+//     for (var i = 0; i < div2.length; i++) {
+//       user_state[i].classList.remove("clicked");
+//     }
+
+//     event.target.classList.add("clicked");
+//   }
+// }
+
+// function init() {
+//   for (var i = 0; i < user_state.length; i++) {
+//     user_state[i].addEventListener("click", handleClick);
+//   }
+// }
+
+// init();
--- js/write.js
+++
@@ -1,1 +0,0 @@
-const link=document.querySelector("a");
